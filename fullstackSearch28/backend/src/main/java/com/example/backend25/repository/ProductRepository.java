700c2e81ce39390eb08be5c56a3b10e5a39943eb
Nicholas Riegel <nicholaspriegel@gmail.com>
2025-02-25 10:15:37 -0500

    new dir

---
+++ fullstackSearch28/backend/src/main/java/com/example/backend25/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend25.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.backend25.models.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {}
